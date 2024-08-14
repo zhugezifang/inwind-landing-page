25c10d6cafe8392b04c465de0ee3a2a2dea1c74f
zhugezifang <xiaoqiu2017wy@163.com>
2024-08-14 05:15:34 +0000

    feat:tj

--- components/common/head.js
+++ components/common/head.js
@@ -37,6 +37,21 @@ export default function CustomHead() {
 		})}
 		
 		<script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-X8FN5Y656M`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-X8FN5Y656M');
+            `,
+          }}
+        />
+		
+		<script
 			dangerouslySetInnerHTML={{
 				__html: `
 				(function() {
