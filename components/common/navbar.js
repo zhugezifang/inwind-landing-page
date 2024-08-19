0b4f82336be92c69080589b27e28c9786820b638
zhugezifang <xiaoqiu2017wy@163.com>
2024-08-19 09:19:03 +0000

    feat:kf

--- components/common/navbar.js
+++ components/common/navbar.js
@@ -1,6 +1,7 @@
 'use client';
 //import Image from 'next/image';
 import { MdMenu } from 'react-icons/md';
+import Image from 'next/image';
 //import { SiGithub } from 'react-icons/si';
 import { useEffect, useState } from 'react';
 import ThemeToggle from './themeToggle';
@@ -68,18 +69,17 @@ export default function Navbar() {
 			</ul>
 
 			<div className='md:w-1/5 flex items-center justify-end gap-2'>
-				{/*
+				
 				<label className='flex items-center justify-center md:bg-base-100 md:rounded-full w-5 md:w-8 h-5 md:h-8 md:shadow-sm md:hover:shadow-md transition-all'>
-					<a
-						aria-label='get template source code'
-						title='get template source code'
-						href='https://github.com/huglemon/inwind-landing-page'
-					>
-						blog
-						<SiGithub size={14} />
+					<a target="_blank" href="https://ko-fi.com/zhugezifang">
+					<Image
+						width={40}
+						height={40}
+						src={'/kf.png'}
+						alt='Buy Me a Coffee at ko-fi.com' />
 					</a>
 				</label>
-				*/}
+				
 				<ThemeToggle />
 				<LangSwitch />
 				<details className='flex md:hidden dropdown dropdown-end'>
