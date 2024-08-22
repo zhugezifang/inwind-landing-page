7e0c04359ff1dc2411a15b0cfaa297077806709b
zhugezifang <xiaoqiu2017wy@163.com>
2024-08-22 14:29:03 +0000

    feat:mt

--- components/home/content.js
+++ components/home/content.js
@@ -4,7 +4,7 @@ import { ContentList } from '@/lib/contentsList';
 export default function Feature({ locale, langName = 'en' }) {
 	let list = ContentList[`Content_${langName.toUpperCase()}`] || [];
 	return (
-		<div className="relative isolate overflow-hidden px-6 py-6 shadow-2xl sm:rounded-3xl sm:px-24 xl:pt-4 flex justify-between items-center">
+		<div className="relative isolate overflow-hidden px-6 py-6 shadow-2xl sm:rounded-3xl sm:px-24 xl:pt-4 flex justify-between items-center mt-4">
             <div className="mx-auto">
                 <div
                     className="mt-3 rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
