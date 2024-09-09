c070934cd3e2fb4b9a2771ed36a8190d7d2fbfc1
zhugezifang <xiaoqiu2017wy@163.com>
2024-09-09 02:11:39 +0000

    feat:url

--- components/common/footer.js
+++ components/common/footer.js
@@ -51,7 +51,7 @@ export default function Footer() {
                         </li>
 
                         <li className="mb-4">
-                            <a href="https://woy.ai/" title="Woy AI Tools Directory" target="_blank" className="hover:underline">Woy AI Tools</a>
+                            <a href="https://bai.tools" target="_blank" className="hover:underline">BAI.tools</a>
                         </li>
 
                     </ul>
