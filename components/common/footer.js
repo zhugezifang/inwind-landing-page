27c76345eff94159b00e56383a6d9f44d6713bf2
zhugezifang <xiaoqiu2017wy@163.com>
2024-09-09 01:56:39 +0000

    feat:url

--- components/common/footer.js
+++ components/common/footer.js
@@ -46,11 +46,8 @@ export default function Footer() {
                             </a>
                         </li>
                         <li className="mb-4">
-                            <a href="https://bottleneck-calculators.online/" target="_blank" className="hover:underline">Bottleneck Calculator
+                            <a href="https://bottleneck-calculators.online/" title="Bottleneck Calculator" target="_blank" className="hover:underline">Bottleneck Calculator
                             </a>
-                        </li>
-                        <li className="mb-4">
-                            <a href="https://tap4.ai/" title="Tap4 AI Tools Directory" target="_blank" className="hover:underline">Tap4 AI Tools Diresctory</a>
                         </li>
                     </ul>
                 </div>
