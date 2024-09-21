208e71aa451ee284dbfa8b3e83889b209e8eda80
zhugezifang <xiaoqiu2017wy@163.com>
2024-09-21 14:06:04 +0000

    feat:tips

--- components/home/calculator.js
+++ components/home/calculator.js
@@ -66,7 +66,7 @@ export default function Calculator({ locale, langName }) {
           
           <form onSubmit={handleSubmit}>
             <div className="relative mb-4">
-                    <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="charlidamelio" className="w-full bg-gray-800 rounded-lg py-3 px-4 text-white focus:outline-none"/>
+                    <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="Enter TikTok username here" className="w-full bg-gray-800 rounded-lg py-3 px-4 text-white focus:outline-none"/>
                     <button className="absolute right-3 top-1/2 transform -translate-y-1/2">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
